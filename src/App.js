330e187e4a59baa8fb22e5af7100afa3e5963327
Zion Gonet <ziongonet@gmail.com>
2022-06-25 04:08:30 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import { Brand, CTA, Navbar} from './components';
+import { Blog, Footer, Features, Possibility, WhatGPT3, Header } from './containers';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
